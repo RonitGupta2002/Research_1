f669ea306ae29f19f19c6250d9b334ba76d9be81
Ronit Gupta <93960710+RonitGupta2002@users.noreply.github.com>
2023-12-27 12:54:36 +0530

    Update step2.py

--- step2.py
+++ step2.py
@@ -2,66 +2,73 @@ import requests
 from bs4 import BeautifulSoup, SoupStrainer
 import adblockparser
 from urllib.parse import urlparse
+import pandas as pd
 
-# Read filter list content from a file
-filter_list_content = r"C:\Users\ronit\OneDrive\Desktop\Automation\filterlists\easylist.txt"  # Replace with your filter list file path
+# Define the filter list paths
+filter_list_paths = [
+    r"C:\Users\ronit\OneDrive\Desktop\Automation\filterlists\easylist.txt",
+    r"C:\Users\ronit\OneDrive\Desktop\Automation\filterlists\easyprivacy.txt",
+    r"C:\Users\ronit\OneDrive\Desktop\Automation\filterlists\antiadblock.txt"
+    # Add paths to additional filter lists as needed
+]
 
+output_file_path = r"C:\Users\ronit\OneDrive\Desktop\Automation\Functional Website\count.xlsx"  # Replace with your desired output file path
+
+def check_ads(url):
+    ad_count = 0
+    ad_details = []
+
+    try:
+        # Create AdblockRules object for each filter list
+        filter_lists = [adblockparser.AdblockRules(path) for path in filter_list_paths]
+
+        # Fetch webpage content
+        response = requests.get(url)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse all tags
+            soup = BeautifulSoup(response.content, features="lxml")
+
+            # Extract all links
+            links = [link.get("href") for link in soup.find_all("a")]
+
+            # Check ads for each individual link against each filter list
+            for source in links:
+                try:
+                    # Fetch content for each link
+                    link_response = requests.get(source)
+                    if link_response.status_code == 200:
+                        link_soup = BeautifulSoup(link_response.content, features="lxml")
+                        
+                        # Batch check URLs against each filter list
+                        for filter_list in filter_lists:
+                            if any(filter_list.should_block(source, url) for filter_list in filter_lists):
+                                ad_count += 1
+
+                                # Extract ad details (title, URL)
+                                ad_title = link_soup.title.text if link_soup.title else "No Title"
+                                ad_details.append({"Title": ad_title, "URL": source})
+                                break  # Move to the next link once an ad is found
+                except requests.exceptions.RequestException:
+                    pass  # Skip if there's an issue with the link
+
+    except Exception as e:
+        print(f"Error checking ads for {url}: {e}")
+
+    return ad_count, ad_details
+
+# Read URLs from input Excel file
+input_file_path = r"C:\Users\ronit\OneDrive\Desktop\Automation\Functional Website\third_party.xlsx"  # Replace with your input file path
 try:
-    # Create AdblockRules object by parsing the filter list content
-    filter_list = adblockparser.AdblockRules(filter_list_content)
+    df = pd.read_excel(input_file_path)
+    ad_data = df['URL'].apply(check_ads)
+    df['Ad_Count'] = [data[0] for data in ad_data]
+    df['Ad_Details'] = [data[1] for data in ad_data]
 
-    # Target webpage
-    url = "https://timesofindia.indiatimes.com/"
-
-    # Fetch webpage content
-    response = requests.get(url)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Use SoupStrainer to parse only 'a' tags
-        parse_only_tags = SoupStrainer('a')
-        soup = BeautifulSoup(response.content, "html.parser", parse_only=parse_only_tags)
-
-        # Extract all links
-        links = [link.get("href") for link in soup.find_all("a")]
-
-        # Remove links having the domain name of the provided URL
-        parsed_url = urlparse(url)
-        domain_to_remove = parsed_url.netloc
-
-        filtered_links = [link for link in links if urlparse(link).netloc != domain_to_remove]
-
-        # Counter for ads found on each individual link
-        ads_count_dict = {}
-
-        # Check ads for each individual link
-        for source in filtered_links:
-            try:
-                # Fetch content for each link
-                link_response = requests.get(source)
-                if link_response.status_code == 200:
-                    link_soup = BeautifulSoup(link_response.content, "html.parser")
-                    # Batch check URLs against filter list rules for each link
-                    if filter_list.should_block(source, url):
-                        ads_count_dict[source] = ads_count_dict.get(source, {"count": 0, "details": ""})
-                        ads_count_dict[source]["count"] += 1
-
-                        # Extract some identifiable feature for the ad (e.g., title, class, id)
-                        ad_title = link_soup.title.text if link_soup.title else "No Title"
-                        ad_details = f"Title: {ad_title}"  # You can extract more details here if available
-                        ads_count_dict[source]["details"] = ad_details
-            except requests.exceptions.RequestException:
-                pass  # Skip if there's an issue with the link
-
-        # Print the count of ads found on each individual link along with details
-        print("Ads count on each individual link:")
-        for link, ad_info in ads_count_dict.items():
-            print(f"{link}: Ads count - {ad_info['count']} - Details: {ad_info['details']}")
-
-    else:
-        print(f"Failed to fetch webpage. Status code: {response.status_code}")
+    # Save results to an output Excel file
+    df.to_excel(output_file_path, index=False)
+    print(f"Results saved to {output_file_path}")
 
 except FileNotFoundError:
-    print("Filter list file not found. Please provide the correct file path.")
-except requests.exceptions.RequestException as e:
-    print("Error fetching webpage:", e)
+    print("Input file not found. Please provide the correct file path.")
